547fa212d0737c14e86c46878d8bfbb1f059b1b5
Alston Liu <liu.als@northeastern.edu>
2025-03-23 11:45:11 -0400

    Changed script to run q1a batch script.

--- q1/q1b.c
+++ q1/q1b.c
@@ -11,7 +11,7 @@ int main() {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Get_processor_name(proc_name, &namelen);
     // if the number of processes is less than the minimum
-    if (size < NUM_PROCESSES) {
+    if (numprocs < NUM_PROCESSES) {
         if (rank == 0) {
             printf("Error: This program requires at least %d processes.\n", NUM_PROCESSES);
         }
@@ -28,7 +28,6 @@ int main() {
     } else {
         MPI_Recv(&value, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         printf("Process %d from Node %s received value: %d\n", rank, proc_name, value);
-        
         
         if (value < NUM_PROCESSES) {
             value++;
