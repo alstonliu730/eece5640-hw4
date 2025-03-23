7ee2c4cab9e3b2bca1fc80bce105b1655dc2d6e4
Alston Liu <liu.als@northeastern.edu>
2025-03-23 00:35:29 -0400

    Implemented q1a using blocking Send and Receive

---
+++ q1/q1a.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "mpi.h"
+
+#define NUM_PROCESSES 64
+int main() {
+    int numprocs, rank, value, namelen;
+    char proc_name[MPI_MAX_PROCESSOR_NAME];
+
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Get_processor_name(proc_name, &namelen);
+
+    if (numprocs < NUM_PROCESSES) {
+        if (rank == 0) {
+            printf("Error: This program requires at least %d processes.\n", NUM_PROCESSES);
+        }
+
+        MPI_Finalize();
+        return 1;
+    }
+
+    // Initial process
+    if (rank == 0) {
+        value = 0;
+        printf("Process %d from Node %s received value: %d\n", rank, proc_name, value);
+        value ++;
+        MPI_Send(&value, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+    } else {
+        MPI_Recv(&value, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process %d from Node %s received value: %d\n", rank, proc_name, value);
+        value++;
+        
+        if (rank < NUM_PROCESSES - 1) {
+            MPI_Send(&value, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+        }
+    }
+    MPI_Finalize();
+    return 0;
+}
